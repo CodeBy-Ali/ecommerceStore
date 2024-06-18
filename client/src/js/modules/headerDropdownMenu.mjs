e4f35799d721df56cf5898313d16d6cba8226316
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-06-18 19:11:38 +0500

    worked on the header ui

--- client/src/js/modules/headerDropdownMenu.mjs
+++ client/src/js/modules/headerDropdownMenu.mjs
@@ -1,22 +1,54 @@
-const closeMenu = (menu) => menu.classList.remove("open");
-const openMenu = (menu) => menu.classList.add("open");
+
+const removeClass = (element,className) => element.classList.remove(className);
+
+const addClass = (element,className) => element.classList.add(className);
+
 const toggleClass = (element, className) => element.classList.toggle(className);
 
+// unHides the detail container of currently focused category.
+const viewFocusCategoryDetail = (focusedCategory, categoryDetailsList) =>{
+  const focusedCategoryName = focusedCategory.getAttribute('data-categoryName');
+  categoryDetailsList.forEach(categoryDetail => {
+    if (categoryDetail.getAttribute('data-detailOf') === focusedCategoryName) {
+      addClass(categoryDetail, 'active');
+    }
+  })
+}
+
+// hides all the  details container of unfocused categories;
+const hideUnFocusedCategoryDetails = (categoriesDetailContainers) => {
+  categoriesDetailContainers.forEach(categoryDetail => removeClass(categoryDetail, 'active'));
+}
+
+//  controls the submenu of the shop drop down;
+const initializeSubDropDownController = () => {
+  const categoriesDetails = document.querySelectorAll('.detail');
+  const categoriesHeading = document.querySelectorAll('.subDropdown_item_category')
+  categoriesHeading.forEach(category => category.addEventListener('mouseenter', (e) => {
+    hideUnFocusedCategoryDetails(categoriesDetails);
+    viewFocusCategoryDetail(e.target,categoriesDetails);
+  }))
+}
+
+// controls the main shop dropdown menu in the header
 const headerDropdownMenuController = () => {
   const shopLink = document.querySelector("[data-shopLink]");
-  const menu = document.querySelector(".header_dropDown_menu");
+  const dropDownContainer = document.querySelector(".dropDown_container");
+  const dropDownMenu = document.querySelector('[data-dropDownMenu]')
   shopLink.addEventListener("mouseenter", (e) => {
-    openMenu(menu), toggleClass(shopLink, "open");
+    addClass(dropDownContainer,"open"), toggleClass(shopLink, "open");
   });
   shopLink.addEventListener("mouseleave", (e) => {
-    closeMenu(menu), toggleClass(shopLink, "open");
+    removeClass(dropDownContainer,'open'), toggleClass(shopLink, "open");
   });
-  menu.addEventListener("mouseenter", (e) => {
-    openMenu(menu), toggleClass(shopLink, "open");
+  dropDownMenu.addEventListener("mouseenter", (e) => {
+    addClass(dropDownContainer,'open'), toggleClass(shopLink, "open");
   });
-  menu.addEventListener("mouseleave", (e) => {
-    closeMenu(menu), toggleClass(shopLink, "open");
+  dropDownMenu.addEventListener("mouseleave", (e) => {
+    removeClass(dropDownContainer,'open'), toggleClass(shopLink, "open");
   });
+
+  initializeSubDropDownController();
 };
 
 export default headerDropdownMenuController;
