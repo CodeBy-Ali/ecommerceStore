6f8c50bbe6b120a79f53b88afa326d57034e56a2
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-07-24 09:01:28 +0500

    feat(cart): Add delete item from cart

--- client/src/typescript/components/Cart/cart.ts
+++ client/src/typescript/components/Cart/cart.ts
@@ -87,34 +87,189 @@ const addProductToCart = async (e: Event) => {
       displayResponseNotification(responseBody?.message, false);
       return;
     }
-    if (!responseBody.cartItems) throw new Error("Failed to get cartItems in response");
-    renderUpdatedCart(responseBody?.cartItems);
+    const cart = responseBody as ICart;
+    if (!cart) throw new Error("Failed to get cart in response");
+    const totalCartItems = cart.items.reduce((total, { quantity }) => total += quantity, 0);
+    renderUpdatedCart(cart.items,totalCartItems);
+    updateHeaderCartQuantity(totalCartItems);
     toggleCartDrawer();
+    innitCartEventListeners();
   } catch (error) {
     console.log(error);
   }
-  // const [productIdField] = cartForm.elements;
 };
 
-// closes and opens the cart menu
-const toggleCartDrawer = (): void => {
-  const cartDrawer = document.querySelector<HTMLElement>("[data-cartDrawer]");
-  const body = document.querySelector<HTMLElement>("body");
-  const overLay = document.querySelector<HTMLElement>("[data-overLay]");
 
-  body && DOMUtils.toggleClass(body, "hideOverflow");
-  cartDrawer && DOMUtils.toggleClass(cartDrawer, "open");
-  overLay && DOMUtils.toggleClass(overLay, "active");
+async function removeProductFromCart(e:Event){
+  const removeButton = e.target as HTMLElement;
+  const productId = removeButton.getAttribute('data-product-id');  
+  
+  try {
+    if (!productId) throw new Error('Failed to get ProductId');
+    const response = await fetch(`/cart/items/${productId}`, {
+      method: "DELETE",
+      headers: {
+        "Accept": "application/json",
+      },      
+    })
+
+    const responseBody = await response.json();
+    if (!response.ok) {
+      displayResponseNotification(responseBody?.message, false);
+      return;
+    }
+    const cart = responseBody as ICart;
+    if (!cart) throw new Error("Failed to get cart in response");
+    const totalCartItems = cart.items.reduce((total, { quantity }) => total += quantity, 0);
+    renderUpdatedCart(cart.items,totalCartItems);
+    updateHeaderCartQuantity(totalCartItems);
+    innitCartEventListeners();
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+function updateHeaderCartQuantity(totalCartItems: number): void{
+  const cartQuantityContainer = document.querySelector<HTMLElement>('header span[data-headerCartQuantity]');
+  DOMUtils.addTextContent(cartQuantityContainer,totalCartItems)
+}
+
+
+function renderUpdatedCart(cartItems: Array<ICartItem>,totalItemsQuantity:number)  {
+  const cartDrawer = document.querySelector("[data-cartDrawer]");
+  const fragment = document.createDocumentFragment();
+  if (!cartDrawer) return console.log("Failed to select cartDrawer section Element");
+  cartDrawer.innerHTML = "";
+
+  if (cartItems.length === 0) {
+    const emptyCartWrapper = document.createElement('div');
+    DOMUtils.addClass(emptyCartWrapper, 'empty_cart_wrapper');
+    fragment.appendChild(emptyCartWrapper);
+
+    emptyCartWrapper.innerHTML = `
+      <button class="cart_close_button" data-cartCloseButton>CLOSE</button>
+        <section class="empty_cart_header_section">
+          <h2>Uh oh,looks like your  bag is empty!</h2>
+          <p>Start philling your shopping bag now</p>
+        </section>
+        <section class="empty_cart_collectionList_section">
+          <ul class="empty_cart_collectionList">
+            <li class="">
+              <a href="">
+                <div>
+                  <img src="/assets/Launch_TheEverythingBar_Ecomm_106.jpeg" alt="">
+                </div>
+                <div>
+                  <h4>HAIR</h4>
+                  <p>Shampoo & conditioner, Refill pods, etc</p>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div>
+                  <img src="/assets/dailyMoisturizer.webp" alt="">
+                </div>
+                <div>
+                  <h4>FACE</h4>
+                  <p>Razors,creams,oils</p>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div>
+                  <img src="/assets/handsoapformbottle.webp" alt="">
+                </div>
+                <div>
+                  <h4>BODY</h4>
+                  <p>Lotions,hands soaps,refills</p>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </section>`;
+  } else {
+    const cartWrapper = document.createElement('div');
+    DOMUtils.addClass(cartWrapper, 'cart-wrapper');
+    fragment.appendChild(cartWrapper);
+
+    cartWrapper.innerHTML = `
+         <section class="cart_header_section">
+           <h2>Bag &lpar;<span class="cart_product_quantity" data-cartItemCount>${totalItemsQuantity}</span>&rpar;</h2>
+           <p class="cart_header_message">Spend $47.02 more or add a subscription to get free shipping!</p>
+           <div class="cart_freeShipping_progressBar_wrapper">
+             <span class="cart_freeShipping_progressBar" data-freeShippingProgressBar></span>
+           </div>
+           <button class="cart_close_button" data-cartCloseButton>CLOSE</button>
+        </section>
+        <section class="cartItems_container" data-cartItemsContainer>
+          <ul>
+           ${cartItems
+        .map(({ product, quantity }) => {
+          return `
+                 <li class="cartItem" data-item-id="${product._id}">
+                   <div class="cartItem_image_container">
+                     <a href="/products/${product.title}">
+                       <img src="${product.image}"  alt="">
+                     </a>
+                   </div>
+                   <div class="cartItem_details">
+                     
+                     <div class="cartItem_heading_container">
+                       <a href="">${product.title}</a>
+                       <p class="cartItem_price">${product.price}</p>
+                     </div>
+     
+                     <div class="cartItem_quantity_container">
+                       <div class="cartItem_quantity_wrapper">
+                         <button class="decrease_cartQuantity_button" data-decreaseCartQuantityBtn>
+                           <span class="visually-hidden">Decrease Quantity for The EveryThing Var</span>
+                           <span>-</span>
+                         </button>
+     
+                         <label class="visually-hidden" for="Quantity-The-EveryThing-Bar">
+                           <span>Product The EveryThing Bar quantity</span>
+                         </label>
+                         <input class="cartItem_quantity_input" type="number" min="1" max="200" value="${quantity}" name="quantity" id="Quantity-The-EveryThing-Bar">
+     
+                         <button data-increaseCartQuantityBtn class="increase_cartQuantity_button">
+                           <span class="visually-hidden">Increase Quantity for The EveryThing Bar</span>
+                           <span>+</span>
+                         </button>
+                       </div>
+     
+                       <div>
+                         <button type="button" data-product-id="${product._id}"  aria-label="Remove ${product.title}" class="remove_cartItem" data-removeCartItemBtn>remove</button>
+                       </div>
+                     </div>
+                   </div>
+                 </li>`;
+        })
+        .join("")}
+          </ul>
+        </section>
+
+        <section class="cart_footer_section">
+          <div class="cart_footer_totalPrize_container">
+            <p>SUBTOTAl</p>
+            <p>
+              $${cartItems.reduce((subtotal, { quantity, product }) => {
+                  return subtotal+=(product.price*quantity)
+                }, 0)
+              }
+            </p>
+          </div>
+          <a href="" class="button--checkout">Checkout</a>
+        </section>`;
+  };
+  cartDrawer.appendChild(fragment);
 };
 
-const Cart = (): void => {
-  const cartButton = document.querySelector("[data-cartButton]");
-  const closeCartButton = document.querySelector("[data-cartCloseButton]");
-  const addToCartBtn = document.querySelector<HTMLElement>("[data-addToCartBtn]");
 
-  cartButton?.addEventListener("click", toggleCartDrawer);
-  closeCartButton?.addEventListener("click", toggleCartDrawer);
-  addToCartBtn?.addEventListener("click", addProductToCart);
-};
+
+
 
 export default Cart;
--- server/src/controllers/cartController.ts
+++ server/src/controllers/cartController.ts
@@ -1,48 +1,39 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
-import Cart,{ICart} from "../model/cartModel.ts";
+import Cart, { ICart, ICartItem } from "../model/cartModel.ts";
 import Product from "../model/productModel.ts";
 import { populateCartItems } from "../utils/utils.ts";
 type ObjectId = mongoose.Types.ObjectId;
 
-
-export const addProductToCart = async (req: Request, res: Response) => {
-  const { productPubId } = req.body;
-  if (!productPubId) {
-    return res.status(400).json({ message: "ProductId is required" });
+export const deleteItem = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  
+  if (!id) {
+    return res.status(400).json({ message: "Product id parameter is required" });
   }
 
-  if (!req.session.user) {
-    return res.status(401).json({message: "Login required to add products to cart"})
-  };
+  if (id.length !== 24) {
+    return res.status(400).json({ message: "Invalid product id" });
+  }
 
   try {
-    const userId = req.session.user as ObjectId;
-    const product = await Product.findOne({ pubId: productPubId });
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
+    const userId = req.session?.user?._id;
+
+    const updatedResult = await Cart.updateOne({ userId: userId }, { $pull: { items: { productId: id } } });
+    if (updatedResult.modifiedCount === 0) {
+      res.status(404).json({message: "Cart not found Or item not in cart"})
     }
-    
-    const productId = product?._id;
+
     const cart = await Cart.findOne({ userId: userId });
-
-    // create new cart if not already present
+    
     if (!cart) {
-      const newCart = createCart(productId,userId);
-      await newCart.save();
-      const cartProducts = await populateCartItems(newCart.items);
-      return res.status(201).json({ cartItems: cartProducts});
+     return res.status(404).json({ message: "Cart not found" });
     }
 
-    // increase the quantity if item is already present in cart else add the item
-    const cartItem = cart.items.find((item) => item.productId.equals(productId));
-    if (cartItem) cartItem.quantity++;
-    else cart.items.push({ productId: productId, quantity: 1 });
-    await cart.save();
-    
-    // store all cart item detail objects in array
-    const cartProducts = await populateCartItems(cart.items);
-    res.status(201).json({ cartItems: cartProducts});
+    const populatedItems = await populateCartItems(cart.items);
+    res.status(200).json({
+      items: populatedItems
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal Server Error" });
@@ -50,15 +41,59 @@ export const addProductToCart = async (req: Request, res: Response) => {
 };
 
 
-function createCart(productId:ObjectId, userId:ObjectId):ICart {
+
+// add product item to user Cart
+export const addItem = async (req: Request, res: Response, next: NextFunction) => {
+  const { productId } = req.body;
+
+  if (!productId) {
+    return res.status(400).json({ message: "Product id is required" });
+  }
+
+  if (productId.length !== 24) {
+    return res.status(400).json({ message: "Invalid product id" });
+  }
+
+  try {
+    const userId = req.session?.user?._id as ObjectId;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const cart = await Cart.findOne({ userId: userId });
+
+    // create new cart if not already present
+    if (!cart) {
+      const newCart = createCart(productId, userId);
+      await newCart.save();
+      const cartProducts = await populateCartItems(newCart.items);
+      return res.status(201).json({ items: cartProducts });
+    }
+
+    // increase the quantity if item is already present in cart else add the item
+    const cartItem = cart.items.find((item) => item.productId.equals(productId));
+    if (cartItem) cartItem.quantity++;
+    else cart.items.push({ productId: productId, quantity: 1 });
+    await cart.save();
+
+    // store all cart item detail objects in array
+    const cartProducts = await populateCartItems(cart.items);
+    res.status(201).json({ items: cartProducts });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+function createCart(productId: ObjectId, userId: ObjectId | string): ICart {
   return new Cart({
     userId: userId,
     items: [
       {
         productId: productId,
-        quantity: 1
-      }
-    ]
-  })
-} 
-
+        quantity: 1,
+      },
+    ],
+  });
+}
