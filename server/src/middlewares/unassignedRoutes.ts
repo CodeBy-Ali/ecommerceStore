cce79ba1a00f6f9491c22e6791d806257d45b5a1
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-07-24 10:32:49 +0500

    feat(api): handle unassigned routes
    Return
    ot found with 404 status code for all unassigned routes

---
+++ server/src/middlewares/unassignedRoutes.ts
@@ -0,0 +1,12 @@
+import { Request,Response,NextFunction,} from "express"
+
+const unassignedRoutesHandler = (req:Request, res:Response, next:NextFunction) => {
+  res.status(404);
+  if  (req.accepts('html')) {
+    res.render('error', { error: '404 not Found' });
+  }else if (req.accepts('application/json, json')) {
+    res.json({ message: `${req.originalUrl} not found` });
+  }else res.send(`404 not Found`)
+}
+
+export default unassignedRoutesHandler;
