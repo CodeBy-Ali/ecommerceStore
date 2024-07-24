8a5bc987fb4852a4331c209d451629f1ea48c5ab
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-07-24 09:28:53 +0500

    feat(api): check if user is authorized

--- server/src/middlewares/authenticate.ts
+++ server/src/middlewares/authenticate.ts
@@ -1,12 +1,17 @@
 import { Request, Response,NextFunction } from "express";
 
 // checks if matching userId is present in the session store
-export const isAuthenticated = (req: Request, res:Response, next:NextFunction) => {
+export const protectRoute = (req: Request, res:Response, next:NextFunction) => {
   if (req.session.user)  next();
-  res.redirect('/login')
+  else res.redirect('/login')
+}
+
+export const isAuthorized = (req: Request, res:Response, next:NextFunction) => {
+  if (req.session.user)  next();
+  else res.status(401).json({ message: "Unauthorized access. Please log in to continue" });
 }
 
 export const redirectIfAuthorized = (req: Request, res:Response, next:NextFunction) => {
   if (!req.session.user) next();
   else res.redirect('/');
-}
+} 
