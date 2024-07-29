6ab42b9d1059e5a0229b51eb856a47575a12d4f1
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-07-29 11:51:29 +0500

    refactor(notification): optimize name from displayResponseNotification to showNotification

--- client/src/typescript/pages/register.ts
+++ client/src/typescript/pages/register.ts
@@ -1,72 +1,65 @@
-import { togglePasswordVisibility,displayResponseNotification } from "../components/utils/util";
+import { togglePasswordVisibility, showNotification } from "../components/utils/util";
 import isFormValid from "../components/validator/validator";
 
-
-
-
-const handleFormSubmit = async(e:Event,registerForm:HTMLFormElement): Promise<void> => {
+const handleFormSubmit = async (e: Event, registerForm: HTMLFormElement): Promise<void> => {
   e.preventDefault();
- 
 
   if (!registerForm || registerForm.elements.length < 4) {
-    throw new Error('Register form elements are missing or form is invalid')
+    throw new Error("Register form elements are missing or form is invalid");
   }
-  
-if (!isFormValid(registerForm)) return;
 
-  const firstNameField = document.querySelector<HTMLInputElement>('[data-firstNameField]');
-  const lastNameField = document.querySelector<HTMLInputElement>('[data-lastNameField]');
-  const emailField = document.querySelector<HTMLInputElement>('[data-emailField]');
-  const passwordField = document.querySelector<HTMLInputElement>('[data-passwordField]');
+  if (!isFormValid(registerForm)) return;
+
+  const firstNameField = document.querySelector<HTMLInputElement>("[data-firstNameField]");
+  const lastNameField = document.querySelector<HTMLInputElement>("[data-lastNameField]");
+  const emailField = document.querySelector<HTMLInputElement>("[data-emailField]");
+  const passwordField = document.querySelector<HTMLInputElement>("[data-passwordField]");
 
   try {
-    const response = await fetch('/account/register', {
+    const response = await fetch("/account/register", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         firstName: firstNameField?.value,
         lastName: lastNameField?.value,
         email: emailField?.value,
         password: passwordField?.value,
-      })
-    })
+      }),
+    });
 
     if (!response.ok) {
       const responseBody = await response.json();
-      displayResponseNotification(responseBody?.message, false);
+      showNotification(responseBody?.message, false);
       return;
     }
 
     if (response.redirected) {
       window.location.href = response.url;
     }
-    
   } catch (error) {
     console.log(error);
   }
-  
-}
+};
 
 //  initialize Register page
 const initRegisterPage = (): void => {
-  const eyeIconButton: HTMLElement | null = document.querySelector('[data-eyeIconButton]');
-  const eyeIcon = document.querySelector('.eye_icon') as HTMLElement;
-  const passwordField = document.querySelector('[data-passwordField]') as HTMLInputElement;
-  const submitButton:HTMLElement | null = document.querySelector('[data-submitButton]');
-  const registerForm = document.querySelector('[data-registerForm]') as HTMLFormElement;
+  const eyeIconButton: HTMLElement | null = document.querySelector("[data-eyeIconButton]");
+  const eyeIcon = document.querySelector(".eye_icon") as HTMLElement;
+  const passwordField = document.querySelector("[data-passwordField]") as HTMLInputElement;
+  const submitButton: HTMLElement | null = document.querySelector("[data-submitButton]");
+  const registerForm = document.querySelector("[data-registerForm]") as HTMLFormElement;
 
   if (!eyeIcon || !passwordField) return;
 
   if (eyeIconButton) {
-    eyeIconButton.addEventListener('click', () => togglePasswordVisibility(eyeIcon, eyeIconButton,passwordField))
+    eyeIconButton.addEventListener("click", () => togglePasswordVisibility(eyeIcon, eyeIconButton, passwordField));
   }
 
   if (submitButton) {
-    submitButton.addEventListener('click',(e)=> handleFormSubmit(e,registerForm));
+    submitButton.addEventListener("click", (e) => handleFormSubmit(e, registerForm));
   }
-
-}
+};
 
 export default initRegisterPage;
--- server/src/middlewares/unassignedRoutes.ts
+++ server/src/middlewares/unassignedRoutes.ts
@@ -4,7 +4,7 @@ const unassignedRoutesHandler = (req:Request, res:Response, next:NextFunction) =
   res.status(404);
   if  (req.accepts('html')) {
     res.render('error', { error: '404 not Found' });
-  }else if (req.accepts('application/json, json')) {
+  }else if (req.accepts(['application/json' ,'json'])) {
     res.json({ message: `${req.originalUrl} not found` });
   }else res.send(`404 not Found`)
 }
