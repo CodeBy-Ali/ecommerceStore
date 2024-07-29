2e5bbce20f8f5953ebb9f0779d0bc333fd0ff4d1
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-07-29 11:52:04 +0500

    feat(cartModel): add timestamps

--- server/src/model/cartModel.ts
+++ server/src/model/cartModel.ts
@@ -1,38 +1,42 @@
-import mongoose,{Schema,model} from "mongoose"
+import mongoose, { Schema, model } from "mongoose";
 import { Document } from "mongoose";
 
-
-
-export interface ICartItem{
+export interface ICartItem {
   productId: mongoose.Types.ObjectId;
-  quantity: number,
+  quantity: number;
 }
 
-export interface ICart extends Document{
-  userId: mongoose.Schema.Types.ObjectId,
-  items: Array<ICartItem>
+export interface ICart extends Document {
+  userId: mongoose.Schema.Types.ObjectId;
+  items: Array<ICartItem>;
 }
 
-
-const cartSchema:Schema = new Schema<ICart>({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-   
-    unique: true,
-  },
-  items: [{
-    productId: {
+const cartSchema: Schema = new Schema<ICart>(
+  {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
       required: true,
+
+      unique: true,
     },
-    quantity: {
-      type: Number,
-      required: true,
-    }
-  }]
-})
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Cart = model<ICart>("cart", cartSchema);
 export default Cart;
