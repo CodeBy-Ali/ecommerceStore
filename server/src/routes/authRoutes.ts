03a24d01e3a136a257abbd7b54efde0373cfec38
Muhammad ALi Hassan <muhammadalihassanofficial@gmail.com>
2024-08-04 10:43:17 +0500

    refactor(api): add renderAuthView
    Render both login and register view using single controller

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,17 +1,16 @@
 import { Router } from "express";
 import { validateAuthBody, validateRegisterBody } from "../middlewares/validator.ts";
 import { redirectIfRegistered } from "../middlewares/authenticate.ts";
-import { renderLoginView, renderRegisterView, registerNewUser, authenticateUser } from "../controllers/authController.ts";
+import { registerNewUser,renderAuthView ,authenticateUser } from "../controllers/authController.ts";
 
 const router: Router = Router();
 
-router.get("/register", renderRegisterView);
+router.get("/register",(req,res,next)=> renderAuthView('register',req,res,next));
 
-router.get("/login", renderLoginView);
+router.get("/login", (req,res,next) => renderAuthView('login',req,res,next));
 
 router.post("/register", validateRegisterBody, registerNewUser);
 
 router.post("/login", validateAuthBody, redirectIfRegistered, authenticateUser);
 
-// router.post('/register', validateAuthInput, registerNewUser);
 export default router;
