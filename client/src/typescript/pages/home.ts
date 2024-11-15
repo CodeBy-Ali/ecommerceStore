397d5ec050582900b55058c1d411f847d057f2a9
Muhammad Ali Hassan <muhammadalihassanofficial@gmail.com>
2024-11-15 17:27:12 +0500

    feat(client): add squalance image animation

--- client/src/typescript/pages/home.ts
+++ client/src/typescript/pages/home.ts
@@ -1,36 +1,67 @@
-
+import DOMUtils from "../components/utils/domUtils";
 
 // returns true of the given element is visible with in viewport else returns false
-const isElementInView = (element:HTMLElement | null):boolean | void => {
+type View = "half" | "full" | "top";
+const isElementInView = (element: HTMLElement, view: View): boolean | void => {
   const windowHeight = window.innerHeight;
-  const elementRect: DOMRect | undefined  = element?.getBoundingClientRect();
-  if (elementRect) {
-    return elementRect.top <= windowHeight && (elementRect.top + elementRect.height) > 0;
-  }
-  return false;
+  const elementRect: DOMRect | undefined = element.getBoundingClientRect();
+
+  const elementTop =
+    elementRect.top +
+    (view == "full"
+      ? elementRect.height
+      : view === "half"
+      ? elementRect.height / 2
+      : 0);
+
+  const elementBottom = elementRect.top + elementRect.height;
+
+  return elementTop <= windowHeight && elementBottom > 0;
 };
 
 // translates the inner scroll in negative X direction about 1/5th of the document Scroll;
-const moveInnerScroller = (innerScroller:HTMLElement | null): void => {
-  if (!isElementInView(innerScroller)) return;
+const moveInnerScroller = (innerScroller: HTMLElement): void => {
+  if (!isElementInView(innerScroller, "top")) return;
   const documentScrollTop: number = document.documentElement.scrollTop;
   if (innerScroller) {
-    innerScroller.style.transform = `translateX(-${Math.floor(documentScrollTop / 5)}px)`;
+    innerScroller.style.transform = `translateX(-${Math.floor(
+      documentScrollTop / 5
+    )}px)`;
   }
 };
 
-const heroImageScroll = ():void=> {
-  const innerScroller: HTMLElement | null = document.querySelector("[data-imageInnerScroller]") as HTMLElement;
-  document.addEventListener("scroll", ():void => moveInnerScroller(innerScroller));
+function moveProductImage(...elements: HTMLElement[]) {
+  elements.forEach((element) => {
+    const isInView = isElementInView(element, "half");
+    if (isInView) {
+      element.style.transform = `translateX(0)`;
+    } else {
+      element.style.transform = `translateX(-8rem)`;
+    }
+  });
+}
+
+const initScrollAnimations = () => {
+  const squalaneProductImage = DOMUtils.getElement<HTMLDivElement>(
+    "[data-squalane-image-container]"
+  );
+  const innerScroller: HTMLElement | null = document.querySelector(
+    "[data-imageInnerScroller]"
+  ) as HTMLElement;
+
+  const windowWidth = window.innerWidth;
+  const laptopScreen = 1400;
+  console.log(windowWidth);
+  if (!squalaneProductImage || !innerScroller) return;
+
+  document.addEventListener("scroll", () => {
+    moveInnerScroller(innerScroller);
+    if (windowWidth > laptopScreen) moveProductImage(squalaneProductImage);
+  });
 };
 
-
-
-
-
-
 const initHomePage = (): void => {
-  heroImageScroll();
-}
+  initScrollAnimations();
+};
 
 export default initHomePage;
